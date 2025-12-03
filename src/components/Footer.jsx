a70d8e3c9045edf61f103f73250bbe506b1ccf6c
Sandanitin <sandanithin2@gmail.com>
2025-12-03 11:36:30 +0530

    facebook

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,12 +31,12 @@ export default function Footer() {
             services to support your well-being.
           </p>
           <div className="flex space-x-4">
-            <a href="#" className="text-white hover:text-white/80 transition-colors">
+            <a href="https://www.facebook.com/profile.php?id=61584752842254" target="_blank" rel="noopener noreferrer" className="text-white hover:text-white/80 transition-colors">
               <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"/>
               </svg>
             </a>
-            <a href="#" className="text-white hover:text-white/80 transition-colors">
+            <a href="https://x.com/SouthernBlvdRx" target="_blank" rel="noopener noreferrer" className="text-white hover:text-white/80 transition-colors">
               <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M12 2C6.477 2 2 6.477 2 12c0 5.523 4.477 10 10 10 5.523 0 10-4.477 10-10 0-5.523-4.477-10-10-10zm-2 15v-4H7v-4h3V8.5c0-1.933 1.567-3.5 3.5-3.5H16v4h-2.5c-.276 0-.5.224-.5.5V11h3l-.5 4h-2.5v4h-4z"/>
               </svg>
